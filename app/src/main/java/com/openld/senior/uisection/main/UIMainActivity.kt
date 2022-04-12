51e8b005bafbfe495b062c219b33c6a2e15146d0
leidongld <394900130@qq.com>
2022-04-12 19:15:49 +0800

    NavigationView SnackBar

--- app/src/main/java/com/openld/senior/uisection/main/UIMainActivity.kt
+++ app/src/main/java/com/openld/senior/uisection/main/UIMainActivity.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import com.openld.senior.R
+import com.openld.senior.uisection.testdrawerlayout.TestDrawerLayoutActivity
+import com.openld.senior.uisection.testnavigationview.TestNavigationViewActivity
 import com.openld.senior.uisection.testrecyclerview.TestRecyclerViewActivity
+import com.openld.senior.uisection.testsnackbar.TestSnackBarActivity
 import com.openld.senior.utils.PageUtils
 
 class UIMainActivity : AppCompatActivity() {
     private lateinit var mBtnRecyclerView: AppCompatButton
+
+    private lateinit var mBtnDrawerLayout: AppCompatButton
+
+    private lateinit var mBtnNavigationView: AppCompatButton
+
+    private lateinit var mBtnSnackBar: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +32,27 @@ class UIMainActivity : AppCompatActivity() {
         mBtnRecyclerView.setOnClickListener {
             PageUtils.jumpToPage(this, TestRecyclerViewActivity::class.java)
         }
+
+        mBtnDrawerLayout.setOnClickListener {
+            PageUtils.jumpToPage(this, TestDrawerLayoutActivity::class.java)
+        }
+
+        mBtnNavigationView.setOnClickListener {
+            PageUtils.jumpToPage(this, TestNavigationViewActivity::class.java)
+        }
+
+        mBtnSnackBar.setOnClickListener {
+            PageUtils.jumpToPage(this, TestSnackBarActivity::class.java)
+        }
     }
 
     private fun initWidgets() {
         mBtnRecyclerView = findViewById(R.id.btn_recycler_view)
+
+        mBtnDrawerLayout = findViewById(R.id.btn_drawer_layout)
+
+        mBtnNavigationView = findViewById(R.id.btn_navigation_view)
+
+        mBtnSnackBar = findViewById(R.id.btn_snack_bar)
     }
 }
