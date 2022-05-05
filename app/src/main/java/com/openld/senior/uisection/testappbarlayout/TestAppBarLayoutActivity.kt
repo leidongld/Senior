1949b1556e3111ffe99e9694edd72eaa8760b39a
leidongld <394900130@qq.com>
2022-05-05 18:53:11 +0800

    学习AppBarLayout+CollapsingToolbarLayout结合使用的效果

--- app/src/main/java/com/openld/senior/uisection/testappbarlayout/TestAppBarLayoutActivity.kt
+++ app/src/main/java/com/openld/senior/uisection/testappbarlayout/TestAppBarLayoutActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import com.openld.senior.R
 
 class TestAppBarLayoutActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_app_bar_layout)
